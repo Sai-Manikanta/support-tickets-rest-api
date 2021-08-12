98cf5ab0cf0aedd79c6161279c476c6b116f9d9e
mani333007 <mani333007@gmail.com>
2021-08-12 22:44:28 +0530

    Datbase conection added

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = process.env.PORT;
-app.listen(port, () => console.log(`Api listening on http://localshost:${port}`));
+// For unhandles syncronous errors
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+  
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+ .then(() => console.log('Connected to Database'))
+ .catch(err => console.log(err))
+
+const port = process.env.PORT || 9000;
+const server = app.listen(port, () => {
+    console.log(`Api listening on http://localshost:${port}`)
+});
+
+// For unhandles asyncronous errors
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+});
